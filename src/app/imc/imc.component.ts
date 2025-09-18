b1768a3156fa5cccfb99e99e7550615c70e82635
Francisco <lysar1x950@gmail.com>
2025-09-18 11:04:41 -0600

    Added peso,altura,imc,estado

--- src/app/imc/imc.component.ts
+++ src/app/imc/imc.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-imc',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './imc.component.html',
-  styleUrl: './imc.component.css'
+  styleUrl: './imc.component.css',
 })
 export class ImcComponent {
-
+  peso: number = 0;
+  altura: number = 0;
+  imc: number = 0;
+  estado: string = '';
 }
