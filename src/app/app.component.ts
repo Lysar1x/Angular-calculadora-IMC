75f0149e0f4be9c953b5105f909d4583f1365b1b
Francisco <lysar1x950@gmail.com>
2025-09-18 11:23:15 -0600

    Created methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ImcComponent } from './imc/imc.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ImcComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'calculadora-imc';
--- src/app/imc/imc.component.ts
+++ src/app/imc/imc.component.ts
@@ -14,4 +14,26 @@ export class ImcComponent {
   altura: number = 0;
   imc: number = 0;
   estado: string = '';
+
+  calcularIMC() {
+    if (this.peso > 0 && this.altura > 0) {
+      this.imc = this.peso / (this.altura * this.altura);
+      this.determinarEstado();
+    } else {
+      this.imc = 0;
+      this.estado = '';
+    }
+  }
+
+  determinarEstado() {
+    if (this.imc < 18.5) {
+      this.estado = 'Bajo peso';
+    } else if (this.imc >= 18.5 && this.imc < 24.9) {
+      this.estado = 'Normal';
+    } else if (this.imc >= 25 && this.imc < 29.9) {
+      this.estado = 'Sobrepeso';
+    } else {
+      this.estado = 'Obesidad';
+    }
+  }
 }
